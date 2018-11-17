77a092e90d82f45f029ee8d0258dbd5ccd00dde5
Dan Boykis <danboykis@gmail.com>
2018-11-17 13:18:20 -0500

    add custom exception handler to NilChannel

--- src/affable_async/channels.clj
+++ src/affable_async/channels.clj
@@ -2,9 +2,32 @@
   (:require [clojure.core.async.impl.channels :as ic]
             [clojure.core.async.impl.protocols :as p]))
 
+(defn- ex-handler [ex]
+  (let [t (Thread/currentThread)]
+    (-> (.getUncaughtExceptionHandler t)
+        (.uncaughtException t ex))
+    nil))
+
+(defn- handle [exh t]
+  (let [result ((or exh ex-handler) t)]
+    (when-not (nil? result)
+      result)))
+
+(defn- put-handler! [val xf exh]
+  (let [retry (volatile! {:val val :xf xf :exh exh})]
+    (while @retry
+      (let [{:keys [val xf exh] :as m} @retry]
+        (vreset! retry nil)
+        (when (some? xf)
+          (locking xf
+            (try
+              ((xf (fn [& _])) nil val))
+            (catch Throwable t
+              (when-some [result (handle exh t)]
+                (vreset! retry (assoc m :val result))))))))))
 
 ;; like ManyToManyChannel but a lot less useful
-(deftype NilChannel [closed xf]
+(deftype NilChannel [closed xf exh]
   ic/MMC
   (cleanup [_] nil)
   (abort [_] nil)
@@ -15,10 +38,8 @@
   (take! [_ _] nil)
   p/WritePort
   (put! [_ val _]
-    (when (some? xf)
-      (locking xf
-        ((xf (fn [& _])) nil val)))
-      (atom (not @closed)))
+    (put-handler! val xf exh)
+    (atom (not @closed)))
   java.lang.Object
   (toString [_] (str "NilChannel<<" "closed:" @closed ">>")))
 
@@ -27,4 +48,6 @@
   ([]
    (nil-chan nil))
   ([xf]
-   (NilChannel. (atom false) xf)))
+   (NilChannel. (atom false) xf nil))
+  ([xf exh]
+   (NilChannel. (atom false) xf exh)))
