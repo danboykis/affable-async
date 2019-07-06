8d5158fe4e22e3ed64824d0750e7f6a9cd461048
Dan Boykis <danboykis@gmail.com>
2019-07-06 13:21:34 -0400

    bumping to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.danboykis/affable-async "0.1.0-SNAPSHOT"
+(defproject com.danboykis/affable-async "0.1.0"
   :description "friendly core.async helpers"
   :url "https://github.com/danboykis/affable-async"
   :license {:name "Unlicense" :url "http://unlicense.org/"}
