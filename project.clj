d881d16486d8dcd3e95dc3b9cc41b9fdf5b7e061
Dan Boykis <danboykis@gmail.com>
2019-07-06 13:19:08 -0400

    marking deps as provided

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "friendly core.async helpers"
   :url "https://github.com/danboykis/affable-async"
   :license {:name "Unlicense" :url "http://unlicense.org/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]])
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/core.async "0.4.500" :scope "provided"]])
