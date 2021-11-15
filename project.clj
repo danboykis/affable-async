ef7ec4e059183d95a6e6f72ba1e86745508efbd3
Dan Boykis <danboykis@gmail.com>
2021-11-15 11:40:36 -0500

    version 0.1.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.danboykis/affable-async "0.1.1-SNAPSHOT"
+(defproject com.danboykis/affable-async "0.1.1"
   :description "friendly core.async helpers"
   :url "https://github.com/danboykis/affable-async"
   :license {:name "Unlicense" :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
-                 [org.clojure/core.async "1.3.618" :scope "provided"]])
+                 [org.clojure/core.async "1.4.627" :scope "provided"]])
